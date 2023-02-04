bae0c1c9a6feb0e15042b45213b65660fa012b5b
Markus Stoff <s52005@edu.campus02.at>
2023-02-04 15:27:44 +0100

    Add tests and include JUnit via maven

---
+++ src/test/java/org/campus02/stoff/BMICalculatorTest.java
@@ -0,0 +1,32 @@
+package org.campus02.stoff;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BMICalculatorTest {
+
+	private BMICalculator bmiCalculator;
+
+	@BeforeEach
+	void setUp() {
+		bmiCalculator = new BMICalculator(180, 1.8);
+	}
+
+	@Test
+	@DisplayName("Konstruktor Test")
+	void constructorTest() {
+		double expectedWeight = 180;
+		double expectedHeight = 1.8;
+
+		assertEquals(expectedWeight, bmiCalculator.getWeightInKg());
+		assertEquals(expectedHeight, bmiCalculator.getHeightInMeters());
+	}
+
+	@Test
+	void result() {
+		
+	}
+}
